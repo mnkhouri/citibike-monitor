a4b233cdfa5b3cc2903352cde28ff776b26266df
Marc Khouri <marc@khouri.ca>
2017-07-28 00:10:52 -0400

    Implement add/remove of stations, make layout better

--- TODO.md
+++ TODO.md
@@ -1,10 +1,6 @@
 ### Next steps:
 
-- Make station status into rows (containers?)
-- Add a `-` button to destroy a container/station
-- Add a `+` button to add a container/station
-    - First step: type in the ID
-    - Next step: show nearest stations based on an entered address
+- Show nearest stations based on an entered address or geolocation
 - Config file for station ID storage
 - Autorefresh
     - Store timestamp and change on each change
--- window.js
+++ window.js
@@ -1,24 +1,90 @@
 const GbfsClient = require('gbfs-client');
 const gbfsClient = new GbfsClient();
 
-// Run this function after the page has loaded
+// JQuery runs this after the page has loaded
+// Alternate syntax for:  $(document).ready(function(){
 $(function () {
-  $('#station-name').text('Loading...');
+  // Get the list of stations
+  gbfsClient.stationInfo()
+    .then(stations => {
+      // Sort the stations by name
+      stations.sort((a, b) => {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+      })
+      // Put together an array of HTML options
+      var options = []
+      for (var station of stations) {
+        options.push(
+          `<option value=${station.station_id}>
+            ${station.name}
+          </option>`
+        )
+      }
+      // Add the HTML to the DOM
+      $('#add-a-station').html(`
+        <form>
+          <select class="form-control">
+            ${options.join(' ')}
+          </select>
+        </form>
+      `);
+      // Add the change handler
+      $('#add-a-station').change(function() {
+        createStation($('#add-a-station option:selected').val())
+      });
+    })
+    .catch(err => $('#add-a-station').text(`Error: ${err.message}`));
 
-  const stationId = '3191';
 
-  gbfsClient.stationInfo(stationId)
-    .then(station => {
-    $('#station-name').text(station.name);
-  }).catch(err => {
-    $('#station-name').text(`Error! ${err.message}`);
-  })
+  const createStation = function(stationId) {
+    const idStation = `station-${stationId}`;
+    const idStationWrap = `station-wrap-${stationId}`;
+    const idStationName = `station-name-${stationId}`;
+    const idStationBikesAvail = `station-bikes-available-${stationId}`;
+    const idStationOverlay = `station-overlay-${stationId}`;
 
-  gbfsClient.stationStatus(stationId)
-    .then(station => {
-    $('#station-bikes-available').text(station.num_bikes_available);
-  }).catch(err => {
-    $('#station-bikes-available').text(`Error! ${err.message}`);
-  })
+    const newStationHtml =
+      `<div class="col-xs-12 col-sm-4 col-md-3 col-lg-2 station" id="${idStation}">
+        <div id="${idStationWrap}">
+          <span id="${idStationName}" class="station-name">Loading station name...</span>
+          <br>
+          Bikes available:
+          <span id="${idStationBikesAvail}" class="station-bikes-available">Loading bike count...</span>
+          <br><br>
+        </div>
+        <div class="x-overlay" id="${idStationOverlay}" />
+      </div>`;
+
+    $('#station-row').append(newStationHtml);
+    $('#' + idStationOverlay).hide();
+
+    $('.station').click(function () { $(this).hide() })
+    $('#' + idStation).hover(
+      function () {
+        $('#' + idStationWrap).hide();
+        $('#' + idStationOverlay).show();
+      },
+      function () {
+        $('#' + idStationWrap).show();
+        $('#' + idStationOverlay).hide();
+      }
+    )
+
+    gbfsClient.stationInfo(stationId)
+      .then(station => {
+      $('#' + idStationName).text(station.name);
+    }).catch(err => {
+      $('#' + idStationName).text(`Error! ${err.message}`);
+    })
+
+    gbfsClient.stationStatus(stationId)
+      .then(station => {
+      $('#' + idStationBikesAvail).text(station.num_bikes_available);
+    }).catch(err => {
+      $('#' + idStationBikesAvail).text(`Error! ${err.message}`);
+    })
+  }
 
 })
